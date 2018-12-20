955a1ddcfb526c7a744af9bc48ed674e9e0a1cd6
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-12-19 22:34:37 -0800

    fixed the routing for the api for post and get requests. need to work on javascript logic on friend find.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,5 +4,14 @@ const friendData = require('../data/friends');
 module.exports = function(app) {
   app.get('/api/friends', function (req,res){
     res.json(friendData);
-  })
+  });
+  app.post("/api/newfriends", function(req, res) {
+    friendData.push(req.body);
+    res.json(true);
+  });
 }
+
+// module.exports = function(app){
+
+//   });
+// }
